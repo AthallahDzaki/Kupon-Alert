69bdd0c3474bb7aa551c3a2a9a621194d8cbb0b0
AthallahDzaki <AthallahDzaki@gmail.com>
2023-08-20 01:20:55 +0700

    Initialize

---
+++ client/script-input.js
@@ -0,0 +1,61 @@
+const socket = io("http://103.175.219.20:3000");
+
+$("#Input").on("submit", function (event) {
+    event.preventDefault();
+    const kupon = $("#Number").val();
+    if (kupon.length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Kupon Kosong',
+            text: "Masukkan Kupon!",
+            timer: 2000,
+            allowOutsideClick: false,
+            allowEscapeKey: false
+        });
+    }
+    else {
+        Swal.fire({
+            icon: 'info',
+            title: 'Menunggu...',
+            text: "Kupon ID : " + kupon,
+            showConfirmButton: false,
+            timer: 3000,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            willClose: () => {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Kupon Berhasil di Kirim ke Server',
+                    text: "KUPON ID : " + kupon,
+                    timer: 4000,
+                })
+            }
+        });
+        socket.emit("PushKupon", kupon);
+    }
+})
+
+$("#Accept").on("click", function (event) {
+    event.preventDefault();
+    socket.emit("DiTerima");
+    Swal.fire({
+        icon: 'success',
+        title: 'Kupon Diterima',
+        showConfirmButton: false,
+        timer: 5000,
+        allowOutsideClick: false,
+        allowEscapeKey: false
+    });
+})
+
+$("#Denied").on("click", function (event) {
+    event.preventDefault();
+    socket.emit("Hangus");
+    Swal.fire({
+        icon: 'error',
+        title: 'Kupon Ditolak',
+        timer: 5000,
+        allowOutsideClick: false,
+        allowEscapeKey: false
+    });
+});
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const socket = require("socket.io");
+const cors = require("cors");
+
+// App setup
+const PORT = 3000;
+const app = express();
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+// Socket setup
+const io = socket(server, {
+    allowEIO3: true,
+    cors: {
+        origin: true,
+        credentials: true
+    }
+});
+let currentKupon = 0;
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+
+  socket.on("PushKupon", function (data) {
+    if(data == 0) return;
+    currentKupon = data;
+    io.emit("AddKupon", currentKupon);
+    console.log("Kupon Push : ", data);
+  });
+
+  socket.on("DiTerima", () => {
+    if(currentKupon == 0) return;
+    currentKupon = 0;
+    io.emit("KuponDiTerima", currentKupon);
+  });
+
+  socket.on("Hangus", () => {
+    if(currentKupon == 0) return;
+    currentKupon = 0;
+    io.emit("KuponHangus", currentKupon);
+  });
+});
